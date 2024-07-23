84c307daf407a9705995df44160d01e7d970bb04
Kevin Du Clos <kduclos@lumcon.edu>
2024-07-23 10:04:19 -0500

    Show 16 most similar images for filename input.

--- video_front_end.py
+++ video_front_end.py
@@ -16,41 +16,43 @@ def read_data():
 
 vecs, names = read_data()
 
-_, fcol2, _ = st.columns(3)
+_, fcol2, fcol3, _, _ = st.columns(5)
 
-scol1, scol2 = st.columns(2)
-
-ch = scol1.button("start / change")
-fs = scol2.button("find similar")
-
-
-if ch:
-    # random_name = names[np.random.randint(len(names))]
-    # image_name = random_name
-    image_name = '2024-04-25_21-22-41-731183_8.jpg'
-    fcol2.image(Image.open(root + image_name))
+# image_name = '2024-04-25_21-22-41-731183_8.jpg'
+image_name = st.text_input("Image name", "2024-04-25_21-22-41-731183_8")
+image_name = image_name + '.jpg'
+try:
+    img = Image.open(f"{root}/{image_name}")
     st.session_state["disp_img"] = image_name
-    st.write(st.session_state["disp_img"])
-if fs:
-    c1, c2, c3, c4, c5 = st.columns(5)
+
+    fcol3.image(Image.open(root + st.session_state["disp_img"]))
+    fcol3.caption(st.session_state["disp_img"])
+
+    a1, a2, a3, a4 = st.columns(4)
+    b1, b2, b3, b4 = st.columns(4)
+    c1, c2, c3, c4 = st.columns(4)
+    d1, d2, d3, d4 = st.columns(4)
+
+    cols = (a1, a2, a3, a4, b1, b2, b3, b4, c1, c2, c3, c4, d1, d2, d3, d4)
+
     idx = int(np.argwhere(names == st.session_state["disp_img"]))
     target_vec = vecs[idx]
 
-    fcol2.image(Image.open(root + st.session_state["disp_img"]))
-    top5 = cdist(target_vec[None, ...], vecs, metric='cosine').squeeze().argsort()[1:6]
-    c1.image(Image.open(root + names[top5[0]]))
-    c2.image(Image.open(root + names[top5[1]]))
-    c3.image(Image.open(root + names[top5[2]]))
-    c4.image(Image.open(root + names[top5[3]]))
-    c5.image(Image.open(root + names[top5[4]]))
 
-    # Show image names
-    c1.text(names[top5[0]])
-    c2.text(names[top5[1]])
-    c3.text(names[top5[2]])
-    c4.text(names[top5[3]])
-    c5.text(names[top5[4]])
+    distances = cdist(target_vec[None, ...], vecs, metric='cosine').squeeze()
+    top_images = distances.argsort()[1:17]
+    top_distances = distances[top_images]
+
+    # Show images
+    for i, col in enumerate(cols):
+        name = names[top_images[0]].replace('.jpg', '')
+        distance = top_distances[i]
+        col.caption(f'{name}')
+        col.caption(f'{distance:.4f}')
+        col.image(Image.open(root + names[top_images[i]]))
+
+except Exception as e:
+    fcol3.caption('Invalid image name')
+    pass
 
 
-
-    
