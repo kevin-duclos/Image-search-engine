7ba373dac22163c4f793370eeb4931e812c6f892
Kevin Du Clos <kduclos@lumcon.edu>
2024-07-23 12:16:56 -0500

    Improve layout and error handling.

--- video_front_end.py
+++ video_front_end.py
@@ -13,69 +13,64 @@ def read_data(root):
     return all_vecs, all_names
 
 
+st.title('Find similar images')
+
 # root = '/Users/kevinadmin/Desktop/PlanktoScope Processing/Test/export_12581_20240719_1809/LUMCON Oyster Larvae Sampling 2024-04-25_1'
 root = st.text_input("File path", "/Users/kevinadmin/Desktop/PlanktoScope Processing/Test/export_12581_20240719_1809/LUMCON Oyster Larvae Sampling 2024-04-25_1")
 try:
     vecs, names = read_data(root)
+    st.subheader(path.basename(root))
 except Exception as e:
     st.warning(e)
 
-
-_, fcol2, fcol3, _, _ = st.columns(5)
-
 # image_name = '2024-04-25_21-22-41-731183_8.jpg'
+top_cols = st.columns(3)
 image_name = st.text_input("Image name", "2024-04-25_21-22-41-731183_8")
 image_name = image_name + '.jpg'
+
 try:
     img = Image.open(path.join(root, image_name))
     st.session_state["disp_img"] = image_name
-
-    fcol3.image(Image.open(path.join(root, st.session_state["disp_img"])))
-    fcol3.caption(st.session_state["disp_img"])
-
-    n_rows = 25
-    n_cols = 4
-    cols = []
-    for _ in range(n_rows):
-        rows = st.columns(n_cols)
-        cols.extend(rows)
-
-    idx = int(np.argwhere(names == st.session_state["disp_img"]).squeeze())
-    target_vec = vecs[idx]
-
-    distances = cdist(target_vec[None, ...], vecs, metric='cosine').squeeze()
-    top_images = distances.argsort()[range(n_rows*n_cols)]
-    top_image_names = names[top_images]
-    top_image_names = np.array([image_name.replace('.jpg', '') for image_name in top_image_names])
-    top_distances = distances[top_images]
-
-    # Show images
-    checks = [None] * len(cols)
-    with st.form(key='image-form'):
-        for i, col in enumerate(cols):
-            name = top_image_names[i]
-            distance = top_distances[i]
-
-            tile = col.container(height=350, border=True)
-            tile.caption(f'{name}')
-            tile.caption(f'distance: {distance:.4f}')
-            try:
-                tile.image(Image.open(path.join(root, names[top_images[i]])))
-            except Exception as e:
-                st.warning(e)
-            checks[i] = tile.checkbox('selected', key=f'check-{i}')
-        submit = st.form_submit_button()
-        if submit:
-            # st.write(checks)
-            selected_images = pd.Series(top_image_names[checks], name=f'similar images for {name}')
-            # selected_images = pd.Series(np.array(name, top_image_names[checks]), name='image names')
-            # pd.concat([name, selected_images], ignore_index=True)
-            st.caption('selected images:')
-            st.dataframe(selected_images, hide_index=True)
-
+    top_cols[1].image(img)
 except Exception as e:
-    # fcol3.caption('Invalid image name')
-    fcol3.caption(e)
-    pass
+    st.warning(e)
 
+n_rows = 25
+n_cols = 4
+cols = []
+for _ in range(n_rows):
+    rows = st.columns(n_cols)
+    cols.extend(rows)
 
+idx = int(np.argwhere(names == st.session_state["disp_img"]).squeeze())
+target_vec = vecs[idx]
+
+distances = cdist(target_vec[None, ...], vecs, metric='cosine').squeeze()
+top_images = distances.argsort()[range(n_rows*n_cols)]
+top_image_names = names[top_images]
+top_image_names = np.array([image_name.replace('.jpg', '') for image_name in top_image_names])
+top_distances = distances[top_images]
+
+# Show images
+checks = [None] * len(cols)
+with st.form(key='image-form'):
+    for i, col in enumerate(cols):
+        name = top_image_names[i]
+        distance = top_distances[i]
+
+        tile = col.container(height=350, border=True)
+        tile.caption(f'{name}')
+        tile.caption(f'distance: {distance:.4f}')
+        try:
+            tile.image(Image.open(path.join(root, names[top_images[i]])))
+        except Exception as e:
+            st.warning(e)
+        checks[i] = tile.checkbox('selected', key=f'check-{i}')
+    submit = st.form_submit_button()
+    if submit:
+        # st.write(checks)
+        selected_images = pd.Series(top_image_names[checks], name=f'similar images for {name}')
+        # selected_images = pd.Series(np.array(name, top_image_names[checks]), name='image names')
+        # pd.concat([name, selected_images], ignore_index=True)
+        st.caption('selected images:')
+        st.dataframe(selected_images, hide_index=True)
