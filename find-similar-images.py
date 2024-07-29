1861783e6b401012e7fc5ef4c904d389992e6dec
Kevin Du Clos <kduclos@lumcon.edu>
2024-07-29 10:58:22 -0500

    Use multiple search images. Return list of unique similar images.

--- find-similar-images.py
+++ find-similar-images.py
@@ -10,11 +10,11 @@ from scipy.spatial.distance import cdist
 
 # %%
 @st.cache_data
-def read_data(root):
-    all_vecs = np.load(f"{root}/all_vecs.npy")
-    all_names = np.load(f"{root}/all_names.npy")
+def read_data(root, search_image_path):
+    all_vecs = np.load(f"{root}/data/all_vecs.npy")
+    all_names = np.load(f"{root}/data/all_names.npy")
     all_names = np.array([image_name.replace('.jpg', '') for image_name in all_names])
-    search_image_path = glob(f"{root}/data/ecotaxa*.tsv")[0]
+    # search_image_path = glob(f"{root}/data/ecotaxa*.tsv")[0]
     search_image_data = read_tsv(search_image_path)
     search_image_names = search_image_data['object_id'].to_numpy()
     search_image_names = np.array([image_name.replace('.jpg', '') for image_name in search_image_names])
@@ -22,20 +22,21 @@ def read_data(root):
     return all_vecs, all_names, search_image_names
 
 st.title('Plankton picker')
-
 # st.session_state["filepath_in"] = st.text_input("File path", "")
-st.session_state["filepath_in"] = st.text_input("File path", "/Users/kevinadmin/Desktop/Image Similarity/LUMCON Oyster Larvae Sampling 2024-04-25_1")
+st.session_state["filepath_images"] = st.text_input("Images path", "/Users/kevinadmin/Desktop/Image Similarity/LUMCON Oyster Larvae Sampling 2024-05-02_1")
+st.session_state["filepath_search"] = st.text_input("EcoTaxa path", "/Users/kevinadmin/Desktop/Image Similarity/LUMCON Oyster Larvae Sampling 2024-05-02_1/data/ecotaxa_export_12759_20240726_1641_bivalve.tsv")
 
 # Get data files
 try:
-    all_vecs, all_names, search_image_names = read_data(st.session_state["filepath_in"])
-    st.subheader(st.session_state["filepath_in"])
+    all_vecs, all_names, search_image_names = read_data(st.session_state["filepath_images"], st.session_state["filepath_search"])
+    st.subheader(st.session_state["filepath_images"])
+    st.subheader(st.session_state["filepath_search"])
 except Exception as e:
     st.warning(e)
 
+
 # %%
 def get_similar_images(vecs, names, search_image_name, n_images):
-    search_image_name = search_image_name
     idx = int(np.argwhere(all_names == search_image_name).squeeze())
     target_vec = vecs[idx]
     distances = cdist(target_vec[None, ...], vecs, metric='cosine').squeeze()
@@ -60,9 +61,13 @@ def get_all_similar_images(search_image_names, n_images):
     # Remove duplicates
     all_top_images.drop_duplicates(subset=['name'], inplace=True)
 
+    # Remove search images
+    all_top_images = all_top_images[~all_top_images['name'].isin(search_image_names)]
+
     # Sort by distance
     all_top_images.sort_values(by='distance', inplace=True)
     all_top_images.reset_index(inplace=True)
+
     return all_top_images
 
 all_top_images = get_all_similar_images(search_image_names, 30)
@@ -78,38 +83,42 @@ def display_images(image_names, n_rows, n_cols, id, image_distances=None):
     checks = [None] * len(cols)
     with st.form(key=f'image-form-{id}'):
         for i, col in enumerate(cols):
-            name = image_names[i]
-            tile = col.container(height=350, border=True)
-            tile.caption(f'{name}')
+            # Allow empty tiles
+            if i < len(image_names):
+                name = image_names[i]
+                tile = col.container(height=350, border=True)
+                tile.caption(f'{name}')
 
-            if image_distances is not None:
-                distance = image_distances[i]
-                tile.caption(f'distance: {distance:.4f}')
+                if image_distances is not None:
+                    distance = image_distances[i]
+                    tile.caption(f'distance: {distance:.4f}')
+                    checks[i] = tile.checkbox('selected', key=f'check-{id}-{i}')
 
-            try:
-                tile.image(Image.open(path.join(st.session_state["filepath_in"], name + '.jpg')))
-            except Exception as e:
-                st.warning(e)
-            checks[i] = tile.checkbox('selected', key=f'check-{id}-{i}')
-        submit = st.form_submit_button()
-        if submit:
-            selected_images = pd.Series(image_names[checks], name=f'selected images')
-            st.caption('selected images:')
-            st.dataframe(selected_images, hide_index=True)
+                try:
+                    tile.image(Image.open(path.join(st.session_state["filepath_images"], name + '.jpg')))
+                except Exception as e:
+                    st.warning(e)
+
+        if image_distances is not None:
+            submit = st.form_submit_button()
+            if submit:
+                selected_images = pd.Series(image_names[checks], name=f'selected images')
+                st.caption('selected images:')
+                st.dataframe(selected_images, hide_index=True)
 
 
 # %% Show search images
-n_rows = 10
 n_cols = 4
 
-# top_image_names = all_top_images['name'].to_numpy()[:n_rows*n_cols]
-# top_image_distances = all_top_images['distance'].to_numpy()[:n_rows*n_cols]
+image_names = search_image_names
+n_rows = np.ceil(image_names.shape[0] / n_cols).astype(int)
+
 st.subheader('Search images')
-display_images(search_image_names, n_rows, n_cols,'search')
+display_images(image_names, n_rows, n_cols,'search')
 
 # %% Show similar images
-n_rows = 10
 n_cols = 4
+n_rows = 10
 
 top_image_names = all_top_images['name'].to_numpy()[:n_rows*n_cols]
 top_image_distances = all_top_images['distance'].to_numpy()[:n_rows*n_cols]
